354585194d59b59b0484cc11b72b318815215a51
Ismael Serrano <ismael.serrano@gmail.com>
2016-08-31 14:50:51 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -39,6 +39,7 @@ ini_setting { 'random ordering':
 # specified in the console for that node.
 
 node default {
+  include users
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
