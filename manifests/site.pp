9db350a7bd29606d3bb9517d1d734fbb9e887010
Ismael Serrano <ismael.serrano@gmail.com>
2016-09-01 10:01:21 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,21 +38,4 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
-  notify { "Hello, my name is ${::hostname}": }
- file { '/etc/motd':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => "Think before you type\n",
- }
- exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
-  path    => '/usr/local/bin',
- }
- include users
- include skeleton
-}
+
