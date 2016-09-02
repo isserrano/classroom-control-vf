75ed7d546c59fe16992a6b587512d219bc9349fa
Ismael Serrano <ismael.serrano@gmail.com>
2016-09-02 14:08:12 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -37,5 +37,9 @@ ini_setting { 'random ordering':
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
-
+node default {
+class { 'nginx':
+root => '/var/www/html',
+}
+}
 
